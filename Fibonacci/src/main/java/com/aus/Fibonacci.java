4cd8a20094d609f05cf7fc79720f22050124c68d
uma1624 <uma.sankar1624@gmail.com>
2021-07-31 23:54:08 +0530

    Fibonacci

---
+++ Fibonacci/src/main/java/com/aus/Fibonacci.java
@@ -0,0 +1,16 @@
+package com.aus;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        int i;
+        int j = 1;
+        int sum = 0;
+
+        for (int k = 0; k < 10; k++) {
+            System.out.print(" " + sum);
+            i = j;
+            j = sum;
+            sum = i + j;
+        }
+    }
+}
